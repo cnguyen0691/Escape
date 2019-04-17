751056d6e10e44041d531ca506fb93a99d411728
cnguyen0691 <chaunguyen0691@gmail.com>
2019-04-17 16:37:52 -0400

    escape

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("*********" );
+        System.out.println("*\t"+ "\t*");
+        System.out.println("*\t"+ "\t*");
+        System.out.println("*\t"+ "\t*");
+        System.out.println("*\t"+ "\t*");
+        System.out.println("*********" );
+
+        System.out.println("  ***" );
+        System.out.println(" *\t"+" *\t");
+        System.out.println("*\t "+"  *\t");
+        System.out.println("*\t "+"  *\t");
+        System.out.println(" *\t"+" *\t");
+        System.out.println("  ***" );
+
+        System.out.println("  *" );
+        System.out.println(" *\t"+"*\t");
+        System.out.println("*\t"+" *\t");
+        System.out.println(" *\t"+"*\t");
+        System.out.println("  *" );
+    }
+}
